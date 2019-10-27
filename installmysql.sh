32b84f305245f6d3a073dbdd1228eab317cb1551
guocodes <awesomeag@hotmail.com>
2019-10-27 23:52:45 +0800

    first commit

---
+++ installmysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+sudo apt-get update
+sudo apt-get install mysql-server
+
+
+sudo mysql -e 'update mysql.user set plugin = "mysql_native_password" where
+user="root"'
+sudo mysql -e 'create user "root"@"%" identified by ""'
+sudo mysql -e 'grant all privileges on *.* to "root"@"%" with grant option'
+sudo mysql -e 'flush privileges'
+sudo service mysql restart
