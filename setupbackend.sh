ed1980881582e965076db38c75f2a4dc6c4eea01
guocodes <awesomeag@hotmail.com>
2019-12-06 03:17:25 +0800

    .

--- setupbackend.sh
+++ setupbackend.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cd ~
-sudo apt-get update
-# git clone --single-branch --branch ag https://github.com/skimkoh/bigdata-goodreads.git
-git clone https://github.com/skimkoh/bigdata-goodreads.git
+# cd ~
+# sudo apt-get update
+# # git clone --single-branch --branch ag https://github.com/skimkoh/bigdata-goodreads.git
+# git clone https://github.com/skimkoh/bigdata-goodreads.git
 
-sudo apt-get install nginx
-sudo apt-get install gunicorn3
-# cd /etc/nginx/sites-enabled/
+# sudo apt-get install nginx
+# sudo apt-get install gunicorn3
+# # cd /etc/nginx/sites-enabled/
+
+# echo "Setting up nginx"
 
 ipaddress=$(curl http://checkip.amazonaws.com)
 
@@ -26,9 +28,11 @@ EOF
 sudo service nginx restart
 
 
-cd bigdata-goodreads/backend
-sudo apt-get install python3-venv
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-gunicorn -w 4 application
+# cd bigdata-goodreads/backend
+# sudo apt-get install python3-venv
+# python3 -m venv venv
+# source venv/bin/activate
+# pip install -r requirements.txt
+# gunicorn -w 4 application
+
+# echo "backend server is up"
