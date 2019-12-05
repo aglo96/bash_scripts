a4b71db44f553ef9169853307eaf98e4cd7a097a
guocodes <awesomeag@hotmail.com>
2019-12-06 00:09:50 +0800

    a

--- installmongo.sh
+++ installmongo.sh
@@ -2,9 +2,7 @@
 cd ~
 sudo apt-get update
 sudo apt install unzip
-wget -c https://istd50043.s3-ap-southeast-1.amazonaws.com/kindle-reviews.zip -O kindle-reviews.zip
-unzip kindle-reviews.zip
-rm -rf kindle_reviews.json
+
 
 wget -c https://istd50043.s3-ap-southeast-1.amazonaws.com/meta_kindle_store.zip -O meta_kindle_store.zip
 unzip meta_kindle_store.zip
